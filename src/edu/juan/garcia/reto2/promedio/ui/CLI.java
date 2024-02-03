c43fe7d38c642f7453bc9dbca6ee175f68860b3e
Juan Francisco García Cortés <juanfran052602@gmail.com>
2024-02-03 13:33:43 -0600

    Programa opcional de calcular promedios: se agrego un nuevo metodo para crear lineas, un mensaje de bienvenida y uno de despedida, esto con el fin de que el programa no se vea tan simple.

--- src/edu/juan/garcia/reto2/promedio/ui/CLI.java
+++ src/edu/juan/garcia/reto2/promedio/ui/CLI.java
@@ -2,12 +2,21 @@ package edu.juan.garcia.reto2.promedio.ui;
 
 import edu.juan.garcia.reto2.promedio.process.Calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CLI {
-
+    public static String generarLineas(char caracter, int longitud){
+        char[] lineaChars = new char[longitud];
+        Arrays.fill(lineaChars,caracter);
+        return new String(lineaChars);
+    }
     public static void LaunchCalculator(){
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println(generarLineas('=',40));
+        System.out.println(" Bienvenido al Calculador de Promedios");
+        System.out.println(generarLineas('=',40));
 
         System.out.println("Ingrese la cantidad de calificaciones a capturar:");
         int cantidadCalificaciones = scanner.nextInt();
@@ -21,5 +30,9 @@ public class CLI {
 
         double promedio = Calculator.calcularPromedio(calificaciones);
         System.out.println("El promedio del alumno es: " +promedio);
+
+        System.out.println(generarLineas('=',40));
+        System.out.println("             ¡Hasta luego!          ");
+        System.out.println(generarLineas('=',40));
     }
 }
