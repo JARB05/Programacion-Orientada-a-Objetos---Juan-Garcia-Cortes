0a4643edc404733a8932047ee45f7c547c9df401
Juan Francisco García Cortés <juanfran052602@gmail.com>
2024-02-17 20:55:05 -0600

    Documentación completada.

--- src/edu/juan/garcia/reto4/data/Passenger.java
+++ src/edu/juan/garcia/reto4/data/Passenger.java
@@ -1,13 +1,27 @@
 package edu.juan.garcia.reto4.data;
 
+/**
+ * Esta clase representa al pasajero que compra un boleto.
+ */
 public class Passenger {
 
+    /**
+     * El nombre completo del pasajero.
+     */
     private String nombreCompleto;
 
+    /**
+     * Crea un nuevo objeto Passenger con el nombre completo proporcionado por el usuario.
+     * @param nombreCompleto El nombre completo del pasajero.
+     */
     public Passenger(String nombreCompleto){
         this.nombreCompleto = nombreCompleto;
     }
 
+    /**
+     * Obtiene el nombre completo del pasajero.
+     * @return El nombre completo del pasajero.
+     */
     public String getNombreCompleto(){
         return nombreCompleto;
     }
--- src/edu/juan/garcia/reto4/process/TicketManager.java
+++ src/edu/juan/garcia/reto4/process/TicketManager.java
@@ -3,14 +3,31 @@ import edu.juan.garcia.reto4.data.Passenger;
 import edu.juan.garcia.reto4.data.Ticket;
 import java.util.ArrayList;
 
+/**
+ * Esta clase gestiona los boletos de compra y proporciona funcionalidades para vender boletos y
+ * mostrar información sobre las ventas.
+ */
 public class TicketManager {
+
+    /** El precio por boleto. */
     private final double precioTicket = 1250.50;
+
+    /** La lista de boletos vendidos. */
     private ArrayList<Ticket> ticketList;
 
+    /**
+     * Crea un nuevo gestor de boletos.
+     */
     public TicketManager(){
         this.ticketList = new ArrayList<>();
     }
 
+    /**
+     * Vende un boleto con el nombre del comprador, la cantidad de boletos y la lista de pasajeros proporcionados.
+     * @param nombreComprador El nombre del comprador del boleto.
+     * @param cantidadBoletos La cantidad de boletos comprados.
+     * @param passengers La lista de pasajeros asociados con el boleto.
+     */
     public void venderBoleto(String nombreComprador, int cantidadBoletos, ArrayList<Passenger> passengers){
         Ticket ticket = new Ticket(nombreComprador, cantidadBoletos);
         for (Passenger passenger : passengers) {
@@ -19,10 +36,18 @@ public class TicketManager {
         ticketList.add(ticket);
     }
 
+    /**
+     * Obtiene la lista de boletos vendidos.
+     * @return La lista de boletos vendidos.
+     */
     public ArrayList<Ticket> getTicketList() {
         return ticketList;
     }
 
+    /**
+     * Muestra los detalles de un boleto.
+     * @param ticket El boleto cuyos detalles se mostrarán.
+     */
     public void displayTicket(Ticket ticket) {
         System.out.println("Nombre del comprador: " + ticket.getNombreComprador());
         System.out.println("Cantidad de boletos: " + ticket.getCantidadBoletos());
@@ -34,6 +59,10 @@ public class TicketManager {
         System.out.println("Importe total: $" + total + "MXN");
     }
 
+    /**
+     * Calcula y muestra el total de todas las compras.
+     * @return El total de todas las compras.
+     */
     public double mostrarTotal() {
     double totalGeneral = 0;
     for (Ticket ticket : ticketList){
