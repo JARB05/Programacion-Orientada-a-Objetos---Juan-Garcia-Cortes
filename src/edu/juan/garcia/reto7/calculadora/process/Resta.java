1023bb0d3e067e9a0a9e75337f9ef53ad46b35ce
Juan Francisco García Cortés <juanfran052602@gmail.com>
2024-03-14 22:50:03 -0600

    Este commit introduce dos clases fundamentales en nuestra calculadora: Suma y Resta. Estas clases son esenciales para realizar las operaciones aritméticas básicas dentro de la aplicación. Ambas clases heredan de la clase abstracta Operacion

---
+++ src/edu/juan/garcia/reto7/calculadora/process/Resta.java
@@ -0,0 +1,8 @@
+package edu.juan.garcia.reto7.calculadora.process;
+
+public class Resta extends Operacion{
+    @Override
+    public double calcular(double x, double y) {
+        return x - y;
+    }
+}
---
+++ src/edu/juan/garcia/reto7/calculadora/process/Suma.java
@@ -0,0 +1,8 @@
+package edu.juan.garcia.reto7.calculadora.process;
+
+public class Suma extends Operacion{
+    @Override
+    public double calcular(double x, double y){
+        return x + y;
+    }
+}
