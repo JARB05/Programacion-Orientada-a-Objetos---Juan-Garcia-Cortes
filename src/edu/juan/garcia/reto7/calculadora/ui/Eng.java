54e868071e9225b36f139dbcb11465db6bcc4f5b
Juan Francisco García Cortés <juanfran052602@gmail.com>
2024-03-23 22:15:52 -0600

    Se añadieron varias clases y metodos las cuales permiten que el usuario pueda escoger entre ver los textos en ingles o español

---
+++ src/edu/juan/garcia/reto7/calculadora/ui/Eng.java
@@ -0,0 +1,32 @@
+package edu.juan.garcia.reto7.calculadora.ui;
+
+public class Eng extends Textos{
+    public Eng(){
+        CALCULADORA = "Calculator";
+        ELECCION = "Choose the operation you want to perform:";
+        SUMA = "a. Addition";
+        RESTA = "b. Subtraction";
+        MULTIPLICACION = "c. Multiplication";
+        DIVISION = "d. Division";
+        MODULO = "e. Modulus";
+        POTENCIA = "f. Power";
+        RAIZ = "g. Square Root";
+        LOGARITMO = "h. Logarithm";
+        SALIR = "i. Exit";
+
+        VALOR1 = "Enter the first value";
+        VALOR2 = "Enter the second value";
+        DIVIDENDO = "Enter the dividend";
+        DIVISOR = "Enter the divisor";
+        BASE = "Enter the value of the base";
+        EXPONENTE = "Enter the value of the exponent";
+        RADICAL = "Enter the value of the radical";
+        INDICE = "Enter the value of the index";
+        ARGUMENTO = "Enter the value of the argument";
+        RESULTADO = "The result of ";
+        ES = " is:";
+
+        DATO_NO_VALIDO = "\nInvalid data:";
+        REINICIE = "\nRestart";
+    }
+}
---
+++ src/edu/juan/garcia/reto7/calculadora/ui/Esp.java
@@ -0,0 +1,32 @@
+package edu.juan.garcia.reto7.calculadora.ui;
+
+public class Esp extends Textos {
+    public Esp() {
+        CALCULADORA = "Calculadora";
+        ELECCION = "Elija la operacion que desea realizar:";
+        SUMA = "a. Suma";
+        RESTA = "b. Resta";
+        MULTIPLICACION = "c. Multiplicación";
+        DIVISION = "d. División";
+        MODULO = "e. Modulo";
+        POTENCIA = "f. Potencia";
+        RAIZ = "g. Raiz";
+        LOGARITMO = "h. Logaritmo";
+        SALIR = "i. Salir";
+
+        VALOR1 = "Ingrese el primer valor";
+        VALOR2 = "Ingrese el segundo valor";
+        DIVIDENDO = "Ingrese el dividendo";
+        DIVISOR = "Ingrese el divisor";
+        BASE = "Ingrese el valor de la base";
+        EXPONENTE = "Ingrese el valor del exponente";
+        RADICAL = "Ingrese el valor del radical";
+        INDICE = "Ingrese el valor del indice";
+        ARGUMENTO = "Ingrese el valor del argumento";
+        RESULTADO = "El resultado de la ";
+        ES = " es:";
+
+        DATO_NO_VALIDO = "\nDato no valido:";
+        REINICIE = "\nReinicie";
+    }
+}
