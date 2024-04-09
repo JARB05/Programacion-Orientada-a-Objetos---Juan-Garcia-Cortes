3813530107cb219a4080a9060f88dbef52cad327
Juan Francisco García Cortés <juanfran052602@gmail.com>
2024-04-08 22:18:20 -0600

    Clase Main y diagrama de clases

---
+++ src/edu/juan/garcia/reto9/Main.java
@@ -0,0 +1,19 @@
+package edu.juan.garcia.reto9;
+
+import edu.juan.garcia.reto9.ui.CLI;
+
+public class Main {
+    /**
+     * Método principal que inicia la aplicación.
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
+    public static void main(String[] args) {
+        try{
+            //Lanza la interfaz de línea de comandos para el programa.
+            CLI.launchApp();
+        } catch (Exception e){
+            //Maneja cualquier error inesperado durante la ejecución.
+            System.out.println("Error inesperado " + e );
+        }
+    }
+}
